bc87e4623aef1484df39c727931de73f49d318b7
Abby <abi.sinta@gmail.com>
2013-07-13 19:45:22 +0700

    Update validate_type.js

--- validate_type.js
+++ validate_type.js
@@ -1,24 +1,21 @@
- 
-  $(document).ready(function(){    
-    
-	  $('#save').click(function(){
-		 var file = $('#image').val();
-		var exts = ['jpg']; 
-      if ( file ) { 
-        var get_ext = file.split('.'); 
-        get_ext = get_ext.reverse(); 
-        if ( $.inArray ( get_ext[0].toLowerCase(), exts ) > -1 ){
-          return true;
-        } else {
-          alert( 'Type must .jpg' );
-        }
-      }
-      else
-      {
+$(document).ready(function(){    
+	$('#save').click(function(){
+	 var file = $('#image').val();
+	 var exts = ['jpg']; 
+      		if ( file ) { 
+        	  var get_ext = file.split('.'); 
+        	  get_ext = get_ext.reverse(); 
+        		if ( $.inArray ( get_ext[0].toLowerCase(), exts ) > -1 ){
+          		  return true;
+        		} else {
+          		  alert( 'Type must .jpg' );
+        		}
+      		}
+      		else
+      		{
 		  alert('Choose Image');
 		 }
 		  return false;
-	  });
-    
+	});
 });
  
